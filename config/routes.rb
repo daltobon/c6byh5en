cc82e7fe6ee0f76ae44042e8ef9eb999d3bfd1ef
daltobon <daniel18.tobon@gmail.com>
2018-04-25 20:17:39 -0500

    Add method create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-     get 'products', to: 'products#index'
+    resources :products, only: [:index, :create]
     end
   end
 
