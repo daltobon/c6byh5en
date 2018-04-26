bbf527dc007a69e60eea3d8475477b0f5db41c9a
daltobon <daniel18.tobon@gmail.com>
2018-04-25 19:16:39 -0500

    Modificando rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'products', to: 'products#index'
+     resources 'products'
     end
   end
-
  
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
